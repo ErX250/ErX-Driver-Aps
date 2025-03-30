a5e87f3ad33e5720033e35689614cbb830b3d4d5
ErX250 <esgmusadrv@gmail.com>
2025-03-30 18:17:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("ErX Driver App Ready! 🚗💨");
+
+// Gelecekte buraya harita, istatistik, API bağlantıları falan eklersin.
+// Şimdilik havamızı atalım:
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Welcome, ErX Driver!");
+});
